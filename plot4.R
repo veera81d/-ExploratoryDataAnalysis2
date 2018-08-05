761c4b298dd1fe58707a3168a04f7d3694026ffb
veera81d <41952588+veera81d@users.noreply.github.com>
2018-08-05 15:52:11 +0400

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,46 @@
+# Download archive file, if it does not exist
+
+if(!(file.exists("summarySCC_PM25.rds") && 
+    file.exists("Source_Classification_Code.rds"))) { 
+    archiveFile <- "NEI_data.zip"
+    if(!file.exists(archiveFile)) {
+        archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+        download.file(url=archiveURL,destfile=archiveFile,method="curl")
+    }  
+    unzip(archiveFile) 
+}
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+head(NEI)
+head(SCC)
+## Load the packages used in the exploratory analysis
+library(ggplot2)
+library(plyr)
+## Converting "year", "type", "Pollutant", "SCC", "fips" to factor
+colToFactor <- c("year", "type", "Pollutant","SCC","fips")
+NEI[,colToFactor] <- lapply(NEI[,colToFactor], factor)
+
+head(levels(NEI$fips))
+## The levels have NA as "   NA", so converting that level back to NA
+levels(NEI$fips)[1] = NA
+NEIdata<-NEI[complete.cases(NEI),]
+colSums(is.na(NEIdata))
+
+## Question 4
+names(SCC)<-gsub("\\.","", names(SCC))
+SCCcombustion<-grepl(pattern = "comb", SCC$SCCLevelOne, ignore.case = TRUE)
+SCCCoal<-grepl(pattern = "coal", SCC$SCCLevelFour, ignore.case = TRUE)
+
+## extracting the SCC in 
+SCCCoalCombustionSCC<-SCC[SCCcombustion & SCCCoal,]$SCC
+NIECoalCombustionValues<-NEIdata[NEIdata$SCC %in% SCCCoalCombustionSCC,]
+NIECoalCombustionTotalEm<-aggregate(Emissions~year, NIECoalCombustionValues, sum)
+
+## Plotting the subset of NEI data with SCC matched with coal and combustion.
+g<-ggplot(aes(year, Emissions/10^5), data=NIECoalCombustionTotalEm)
+g+geom_bar(stat="identity",fill="grey",width=0.75) +
+  guides(fill=FALSE) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
